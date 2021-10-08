d7ca77069a51dea9e928ba8021604e07c804cc9d
sumanta <sumanta111195@gmail.com>
2021-10-08 14:43:42 +0530

    Snackbars

---
+++ app/src/main/java/com/sumanta/tastrow/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.sumanta.tastrow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sumanta.tastrow.ui.theme.TastRowTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Snackbaredettext()
+        }
+    }
+}
+
+@Composable
+fun Snackbaredettext() {
+    val scaffoldState = rememberScaffoldState()
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp)
+            ) {
+            TextField(
+                value = textFieldState,
+                label = {
+                    Text(text = "Enter your name")
+                },
+                onValueChange = {
+                    textFieldState = it
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                }
+            }) {
+                Text(text = "Pls greet me")
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Snackbaredettext()
+    }
+
+
+}
